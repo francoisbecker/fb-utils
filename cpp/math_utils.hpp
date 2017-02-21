7150170c9db5ee5a2853d596b6b3f836d58f57bc
François Becker <francois.becker@gmail.com>
2017-02-21 13:20:45 +0100

    Math utils: added fast log functions

--- cpp/math_utils.hpp
+++ cpp/math_utils.hpp
@@ -190,6 +190,53 @@ namespace mu
     {
         return pVal * pVal;
     }
+    
+    //==============================================================================
+    /**
+     @copyright public domain
+     */
+    inline float fast_log2(float val)
+    {
+        union {float f; int i;} t;
+        t.f = val;
+        int * const exp_ptr = &t.i;
+        int x = *exp_ptr;
+        const int log_2 = ((x >> 23) & 255) - 128;
+        x &= ~(255 << 23);
+        x += 127 << 23;
+        *exp_ptr = x;
+        
+        val = ((-1.0f/3) * t.f + 2) * t.f - 2.0f/3;
+        
+        return (val + log_2);
+    }
+    
+    /**
+     @copyright public domain
+     */
+    inline float fast_log(const float val)
+    {
+        return (fast_log2(val) * 0.69314718f);
+    }
+    
+    /**
+     @copyright public domain
+     */
+    inline float fast_log10(const float val)
+    {
+        return fast_log2(val) / 3.312500f;
+    }
+    
+    //==============================================================================
+    inline float fast_gainToDB(const float g)
+    {
+        return 20.f * fast_log10(g);
+    }
+    
+    inline float fast_powToDB(const float g)
+    {
+        return 10.f * fast_log10(g);
+    }
 }
 
 #endif
