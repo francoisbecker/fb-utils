c660ae164a2bbdae380f4c74e05fd3cf238ff521
François Becker <francois.becker@gmail.com>
2018-01-23 18:12:37 +0100

    fbu_assert_cpp11.hpp added.

---
+++ cpp/fbu_assert_cpp11.hpp
@@ -0,0 +1,37 @@
+#ifndef FBU_ASSERT_CPP11_HPP_INCLUDED
+#define FBU_ASSERT_CPP11_HPP_INCLUDED
+
+/**
+ @file fbu_assert_cpp11.hpp
+ @author François Becker
+
+MIT License
+
+Copyright (c) 2018 François Becker
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#define xstr(s) str(s)
+#define str(s) #s
+static_assert(__cplusplus >= 201103L, xstr(__cplusplus)" i.e. C++ version < C++11, sad");
+#undef xstr
+#undef str
+
+#endif
