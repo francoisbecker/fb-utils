21554ee2c625883be49f8ff278adcd5bfddc520d
François Becker <francois.becker@gmail.com>
2018-04-19 19:07:51 +0200

    test_filesystem_utils.cpp updated.

--- test/test_filesystem_utils.cpp
+++ test/test_filesystem_utils.cpp
@@ -2,14 +2,17 @@
 
 #include "tests_common.hpp"
 
-CASE( "fbu::filesystem::isPOSIXFullyPortableFileName()" )
+CASE( "fbu::fs::isPOSIXFullyPortableFileName()" )
 {
-    EXPECT( fbu::filesystem::isPOSIXFullyPortableFileName("ehfindjfqnsdl872394kfnlze") );
-    EXPECT( fbu::filesystem::isPOSIXFullyPortableFileName("._djfqklsdf-dksfqshdjfkl6736824_sdfhjslf.dfnsjdlf.skfhdls") );
-    EXPECT( ! fbu::filesystem::isPOSIXFullyPortableFileName("") );
-    EXPECT( ! fbu::filesystem::isPOSIXFullyPortableFileName("-hyphen.begin") );
-    EXPECT( ! fbu::filesystem::isPOSIXFullyPortableFileName("/test") );
-    
+    EXPECT( fbu::fs::isPOSIXFullyPortableFileName("ehfindjfqnsdl872394kfnlze") );
+    EXPECT( fbu::fs::isPOSIXFullyPortableFileName("._djfqklsdf-dksfqshdjfkl6736824_sdfhjslf.dfnsjdlf.skfhdls") );
+    EXPECT( ! fbu::fs::isPOSIXFullyPortableFileName("") );
+    EXPECT( ! fbu::fs::isPOSIXFullyPortableFileName("-hyphen.begin") );
+    EXPECT( ! fbu::fs::isPOSIXFullyPortableFileName("/test") );
+}
+
+CASE("fbu::fs::setFileContents() and fbu::fs::getFileContents()")
+{
     std::string lTestPath = "/tmp/lorem.test.txt";
     std::string lFileContents = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     EXPECT( fbu::fs::setFileContents(lTestPath.c_str(), lFileContents.c_str()) );
